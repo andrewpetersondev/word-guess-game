0ed60b9677de0de3eeb3c99634c095bf89f62db7
Andrew P <andrewpetersondev@gmail.com>
2019-05-20 14:41:38 -0500

    removed userGuesses validation, the only problem remaining is that the order of guesses are important for winning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@ var gameWords = ["friends", "ape", "dog", "cat", "gif", "top", "true", "false", 
 
 // random word 
 var computerWord = gameWords[Math.floor(Math.random() * gameWords.length)];
+
+// print computer word to console
 console.log(computerWord);
 
 // only these inputs are valid
@@ -11,15 +13,16 @@ var validInputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 
 // User Inputs and Display
 
-// use string concatenation on this varible to form the whole word and compare to the gameWord
-var rightGuessLetter = ""; 
+// use string concatenation on this varible to form the whole word and compare to the computer word
+var rightGuessLetter = "";
 
 // use string concatenation on this variable to display wrong guesses
+var wrongGuessLetter = "";
 
-var wrongGuessLetter = ""; 
-
-// store user guesses
-var userGuesses = [];
+// store user guesses so that user cant guess the same letter more than one time
+// this is not part of phils solution
+// remove this for now
+// var userGuesses = [];
 
 // stores chosen word in an  array?
 var boardGame = [];
@@ -29,7 +32,8 @@ for (var i = 0; i < computerWord.length; i++) {
     boardGame[i] = "_";
 }
 
-var userInput = ""; // stores the letter key pressed by user // should this variable be stored globally?
+// stores the letter key pressed by user // should this variable be stored globally?
+var userInput = "";
 
 // Game Counter
 var wins = 0;
@@ -38,11 +42,11 @@ var guessCount = 9;
 
 // ====================== FUNCTIONS ========================
 // function initializeGame() {
-//     document.getElementById("wins").innerHTML = wins;
-//     document.getElementById("losses").innerHTML = losses;
-//     document.getElementById("guess-counter").innerHTML = guessCount;
-//     document.getElementById("wrong-guesses").innerHTML = wrongGuessLetter;
-//     document.getElementById("user-guesses").innerHTML = "<b>You already guessed :</b>  " + userGuesses + " ";
+// document.getElementById("wins").innerHTML = wins;
+// document.getElementById("losses").innerHTML = losses;
+// document.getElementById("guess-counter").innerHTML = guessCount;
+// document.getElementById("wrong-guesses").innerHTML = wrongGuessLetter;
+
 // }
 
 // initializeGame();
@@ -58,8 +62,9 @@ function resetGame() {
     for (var i = 0; i < computerWord.length; i++) {
         boardGame[i] = "_";
     }
-    // userInput = "";
-    console.log(computerWord);
+    userInput = "";
+    console.log("WinCount: " + wins + " | LossCount: " + losses + " | GuessCounter: " + guessCount);
+    console.log("New Word: " + computerWord);
 }
 
 // =================== MAIN PROCESS ==============================
@@ -72,80 +77,85 @@ document.onkeyup = function (event) {
     var displayBoardDiv = document.getElementById("computer-word");
 
     // input correct 
+    // join(" ") gets rid of comma seperator from from the array with the empty string 
+    // default array has comma separators
+    // code on line below initially displays the board game with underscores
     displayBoardDiv.textContent = boardGame.join(" ");
 
     // make sure user input is a letter
     if (validInputs.indexOf(userInput) !== -1) {
 
         // make sure the user can only enter the same input once
-        if (userGuesses.indexOf(userInput) === -1) {
+        // take this out for now
+        // if (userGuesses.indexOf(userInput) === -1) {
 
-            // insert userInput into userGuesses array
-            userGuesses.push(userInput);
-            // console.log(userGuesses);
+        // insert userInput into userGuesses array
+        // userGuesses.push(userInput);
+        // console.log(userGuesses);
 
 
-            // for (i = 0; i < computerWord.length; i++) {
-            //     if (computerWord[i] == userInput) {
-            //         boardGame[i] == userInput;
-            //     }
-            // }
+        // for (var i = 0; i < computerWord.length; i++) {
+        //     if (computerWord[i] == userInput) {
+        //         boardGame[i] == userInput;
+        //     }
+        // }
 
-            // check if userInput is in computer word array
-            if (computerWord.indexOf(userInput) !== -1) {
+        // check if userInput is in computer word array
+        // indexOf returns -1 if value is not in array
+        if (computerWord.indexOf(userInput) !== -1) {
 
-                // replaces the underscore with the correct guess letter
-                boardGame[computerWord.indexOf(userInput)] = userInput;
+            // replaces the underscore with the correct guess letter
+            boardGame[computerWord.indexOf(userInput)] = userInput;
 
-                displayBoardDiv.textContent = boardGame.join(" ");
+            displayBoardDiv.textContent = boardGame.join(" ");
 
-                // string concatenation to form computer pick word
-                rightGuessLetter = rightGuessLetter + userInput;
+            // string concatenation to form computer pick word
+            rightGuessLetter = rightGuessLetter + userInput;
 
-                // player wins
-                if (computerWord === rightGuessLetter) {
-                    // increment wins by 1
-                    wins++;
-                    // update wins counter on html page
-                    document.getElementById("wins").innerHTML = wins;
-                    // reset the game
-                    resetGame();
-                }
-
-            }
-            else {
-                // decrement guess count by 1
-                guessCount--;
-
-                // update guess counter in html
-                document.getElementById("guess-counter").innerHTML = guessCount;
-
-                // store wrong guess in a string 
-                wrongGuessLetter = wrongGuessLetter + userInput + " , ";
-
-                console.log(wrongGuessLetter);
-
-                // display wrong guesses on html
-                var outputWrongDiv = document.getElementById("wrong-guesses");
-
-                outputWrongDiv.textContent = wrongGuessLetter;
-
-            }
-            // player loses 
-            if (guessCount <= 0) {
-                // increment losses by 1
-                losses++;
-                // update losses in html
-                document.getElementById("losses").innerHTML = losses;
-                // restart the game
+            // player wins
+            if (computerWord === rightGuessLetter) {
+                // increment wins by 1
+                wins++;
+                // update wins counter on html page
+                document.getElementById("wins").innerHTML = wins;
+                // reset the game
                 resetGame();
             }
-        } // make sure the user can only enter the same input once
-        else {
-            alert("you already selected that, guess again");
+
         }
-    } // make sure user inputs a valid letter
+        else {
+            // decrement guess count by 1
+            guessCount--;
+
+            // update guess counter in html
+            document.getElementById("guess-counter").innerHTML = guessCount;
+
+            // store wrong guess in a string 
+            wrongGuessLetter = wrongGuessLetter + userInput + " , ";
+
+            console.log(wrongGuessLetter);
+
+            // display wrong guesses on html
+            var outputWrongDiv = document.getElementById("wrong-guesses");
+
+            outputWrongDiv.textContent = wrongGuessLetter;
+
+        }
+        // player loses 
+        if (guessCount <= 0) {
+            // increment losses by 1
+            losses++;
+            // update losses in html
+            document.getElementById("losses").innerHTML = losses;
+            // restart the game
+            resetGame();
+        }
+    // } // make sure the user can only enter the same input once
+    // else {
+    //     alert("you already selected that, guess again");
+    // }
+} // make sure user inputs a valid letter
     else {
-        alert("that is not a valid input, guess again");
-    }
+    alert("that is not a valid input, guess again");
+}
 };

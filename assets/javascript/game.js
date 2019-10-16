f103076a19c3ce78120882ae3a099802453f47b5
Andrew P <andrewpetersondev@gmail.com>
2019-10-16 17:39:34 -0500

    started refactoring code to use a game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,138 +1,148 @@
-// console.log("beginning of logic");
+// game object
+var wordGuessGame = {
 
-// GLOBAL VARIABLES 
-// ================================================================================
-
-// arrays and variables to hold data
-var gameWords = ["blockchain", "ibm", "google", "amazon", "developer", "cryptocurrency", "cloud", "quantum computing", "artificial intelligence", "linux", "kali", "robots", "decentralization"];
-var computerWord = "";
-var lettersInComputerWord = [];
-var numBlanks = 0;
-var gameBoard = []; // i _ _
-var wrongGuesses = [];
-
-// Counters
-var wins = 0;
-var losses = 0;
-var guessCount = 9;
-
-// FUNCTIONS 
-// ================================================================================
-
-function startGame() {
-    console.log("startGame() called");
-
-    // set 
-    computerWord = gameWords[Math.floor(Math.random() * gameWords.length)];
-    lettersInComputerWord = computerWord.split('');
-    numBlanks = lettersInComputerWord.length;
-
-    // reset 
-    guessCount = 9;
-    wrongGuesses = [];
-    gameBoard = [];
-
-    // populate game board
-    for (var i = 0; i < numBlanks; i++) {
-        gameBoard.push("_");
-    }
-
-    // display to html
-    // .join() joins array elements into a string
-    document.getElementById("guess-counter").innerHTML = guessCount;
-    document.getElementById("computer-word").innerHTML = gameBoard.join('  ');
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-
-    //  test / debug
-    console.log(computerWord);
-    console.log(lettersInComputerWord);
-    console.log(numBlanks);
-    console.log(gameBoard);
-};
-
-function checkLetters(letter) {
-   
-    // testing and debugging
-    console.log("checkLetters(letter) called");
-    console.log(letter);
-
-    // toggle boolean
-    var letterInWord = false;
-
-    // check if a letter is in game word
-    for (var i = 0; i < numBlanks; i++) {
-        if (computerWord[i] === letter) {
-            letterInWord = true;
+    // word objects
+    gameWords = {
+        blockchain: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        ibm: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        google: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        amazon: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        developer: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        cryptocurrency: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        nasa: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        robots: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        cloud: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
         }
-    }
+    },
 
-    // check where letters are in the game word then populate array
-    if (letterInWord) {
-        for (var j = 0; j < numBlanks; j++) {
-            if (computerWord[j] === letter) {
-                gameBoard[j] = letter;
+    // variables
+    // ==========================================================================
+    computerWord = "",
+    lettersInComputerWord =[],
+    numBlanks = 0,
+    gameBoard =[],
+    wrongGuesses =[],
+    wins = 0,
+    losses = 0,
+    guessCount = 9,
+
+    // methods
+    // ==========================================================================
+    startGame = function () {
+        // set 
+        computerWord = gameWords[Math.floor(Math.random() * gameWords.length)];
+        lettersInComputerWord = computerWord.split('');
+        numBlanks = lettersInComputerWord.length;
+        // reset 
+        guessCount = 9;
+        wrongGuesses = [];
+        gameBoard = [];
+        // populate game board
+        for (var i = 0; i < numBlanks; i++) {
+            gameBoard.push("_");
+        }
+        // display to html
+        document.getElementById("guess-counter").innerHTML = guessCount;
+        document.getElementById("computer-word").innerHTML = gameBoard.join('  ');
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("losses").innerHTML = losses;
+    },
+
+    checkLetters = function (letter) {
+
+        // toggle boolean
+        var letterInWord = false;
+
+        // check if a letter is in game word
+        for (var i = 0; i < numBlanks; i++) {
+            if (computerWord[i] === letter) {
+                letterInWord = true;
             }
         }
-    }
+        
+        // check where letters are in the game word then populate array
+        if (letterInWord) {
+            for (var j = 0; j < numBlanks; j++) {
+                if (computerWord[j] === letter) {
+                    gameBoard[j] = letter;
+                }
+            }
+        }
+        
+        // letter was not in word
+        else {
+            wrongGuesses.push(letter);
+            guessCount--;
+        }
 
-    // letter was not in word
-    else {
-        wrongGuesses.push(letter);
-        guessCount--;
-    }
+    },
 
-    // testing and debugging
-    console.log(gameBoard);
+    gameOver = function () {
+        console.log("Win Count: " + wins + " | Loss Count: " + losses + " | Guesses Left: " + guessCount);
+        // update html
+        document.getElementById("guess-counter").innerHTML = guessCount;
+        document.getElementById("computer-word").innerHTML = gameBoard.join(' ');
+        document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(' ');
+        // user wins
+        if (lettersInComputerWord.toString() === gameBoard.toString()) {
+            wins++;
+            // alert("you win");
+            document.getElementById("wins").innerHTML = wins;
+            startGame();
+        }
+        // user losses
+        else if (guessCount <= 0) {
+            losses++;
+            alert("you lose");
+            document.getElementById("losses").innerHTML = losses;
+            startGame();
+        }
+    }
 
 };
 
-function gameOver() {
-
-    // testing / debugging
-    console.log("gameOver() called");
-    console.log("Win Count: " + wins + " | Loss Count: " + losses + " | Guesses Left: " + guessCount);
-
-    // update html
-    document.getElementById("guess-counter").innerHTML = guessCount;
-    document.getElementById("computer-word").innerHTML = gameBoard.join(' ');
-    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(' ');
-
-    // user wins
-    if (lettersInComputerWord.toString() === gameBoard.toString()) {
-        wins++;
-        // alert("you win");
-        document.getElementById("wins").innerHTML = wins;
-        startGame();
-    }
-    // user losses
-    else if (guessCount <= 0) {
-        losses++;
-        alert("you lose");
-        document.getElementById("losses").innerHTML = losses;
-        startGame();
-    }
-
-}
-
 // MAIN PROCESS
-// ================================================================================
+// ==========================================================================
 
-startGame();
+wordGuessGame.startGame();
 
-// initiates the function for capturing key clicks
 document.onkeyup = function (event) {
-
-    // converts key clicks to lowercase letters and stores them in global variable
-    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
-
-    // testing / debugging
-    console.log(userInput);
-
-    // runs check letters function with data from letters guessed string
+    wordGuessGame.userInput = String.fromCharCode(event.keyCode).toLowerCase();
     checkLetters(userInput);
-
-    // runs everything else
     gameOver();
-
 };
